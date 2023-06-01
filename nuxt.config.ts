5281c6cb1c2a1ba958f35b74c0cf15d86ba5dd46
Vladislav Popov <v.popov@rootmedia.ru>
2023-06-01 11:54:38 +0300

    feat:init

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    imports: {
+        dirs: ["store"],
+    },
+    css: ["@/assets/styles/main.less"],
+    modules: [
+        ["nuxt-icon"],
+        ["@vueuse/nuxt"],
+        ["@nuxt/image-edge"],
+        [
+            "@pinia/nuxt",
+            {
+                autoImports: [
+                    'storeToRefs',
+                    // automatically imports `defineStore`
+                    "defineStore", // import { defineStore } from 'pinia'
+                    // automatically imports `defineStore` as `definePiniaStore`
+                    ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+                ],
+            },
+        ],
+    ],
+    runtimeConfig: {
+        url: process.env.URL,
+
+    },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                less: {
+                    additionalData: `@import "@/assets/styles/_var.less" ;@import "@/assets/styles/_mixins.less" ;`,
+                },
+            },
+        },
+    },
+})
